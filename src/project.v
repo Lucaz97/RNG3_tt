4e3217a87e614df0ca2627fb2064c6c8e3d1b6ed
Lucaz97 <lc4976@nyu.edu>
2024-03-29 11:57:15 -0400

    initiated with approx entropy

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `define default_netname none
 
-module tt_um_example (
+module tt_um_rng_3_lucaz97 (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -16,9 +16,53 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
+      approx approx_inst (
+      .clk(clk),
+      .rst_n(rst_n),
+      .is_random_rsc_dat(uo_out[0]),
+      .is_random_triosy_lz(),
+      .valid_rsc_dat(uo_out[1]),
+      .valid_triosy_lz(),
+      .epsilon_rsc_dat(ui_in[0]),
+      .epsilon_triosy_lz()
+    );
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  assign uo_out[7:2s] = 0;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
   assign uio_oe  = 0;
 
+endmodule
+
+
+// ------------------------------------------------------------------
+//  Design Unit:    approx
+// ------------------------------------------------------------------
+
+
+module approx (
+  clk, rst_n, is_random_rsc_dat, is_random_triosy_lz, valid_rsc_dat, valid_triosy_lz,
+      epsilon_rsc_dat, epsilon_triosy_lz
+);
+  input clk;
+  input rst_n;
+  output is_random_rsc_dat;
+  output is_random_triosy_lz;
+  output valid_rsc_dat;
+  output valid_triosy_lz;
+  input epsilon_rsc_dat;
+  output epsilon_triosy_lz;
+
+
+
+  // Interconnect Declarations for Component Instantiations 
+  approx_core approx_core_inst (
+      .clk(clk),
+      .rst_n(rst_n),
+      .is_random_rsc_dat(is_random_rsc_dat),
+      .is_random_triosy_lz(is_random_triosy_lz),
+      .valid_rsc_dat(valid_rsc_dat),
+      .valid_triosy_lz(valid_triosy_lz),
+      .epsilon_rsc_dat(epsilon_rsc_dat),
+      .epsilon_triosy_lz(epsilon_triosy_lz)
+    );
 endmodule
